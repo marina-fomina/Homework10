5995a17eeb9f2894ff2b509e8bcbaa7a999cbc32
msfom <marinafomina1994@gmail.com>
2022-10-01 12:31:18 +0300

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Author firstAuthor = new Author("Федор", "Достоевский");
+        Author secondAuthor = new Author("Николай", "Гоголь");
+
+        Book firstBook = new Book("Преступление и наказание", firstAuthor, 2010);
+        Book secondBook = new Book("Ревизор", secondAuthor, 2005);
+
+        System.out.println("Книга 1: " + firstBook.getBookName() + ". Автор: " + firstAuthor.getAuthor() + ". Год публикации: " + firstBook.getPublishingYear());
+        System.out.println("Книга 2: " + secondBook.getBookName() + ". Автор: " + secondAuthor.getAuthor() + ". Год публикации: " + secondBook.getPublishingYear());
+
+        firstBook.setPublishingYear(2015);
+        System.out.println("Новый год публикации книги 1 - " + firstBook.getPublishingYear());
     }
 }
